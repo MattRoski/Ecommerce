9217a62f3571c59146ecb0600aa7da0f9ac12e8e
MattRoski <mattroski88@gmail.com>
2022-09-06 17:45:48 -0500

    Can get product repo and category as json

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,16 +3,17 @@ import {HttpClient} from '@angular/common/http'
 import {Observable } from 'rxjs';
 import { Product } from '../common/product';
 import {map} from 'rxjs/operators';
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({ //Says here's a class you can inject as a dependency into another component or class..
+  providedIn: 'root'  //the provided end route means that this class be injected globally..not specific to a given module
+})  //can use it anywhere within this given angular app
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'; //added
 
   constructor(private httpClient: HttpClient) { } //added private httpClient: HttpClient
   
-    getProductList(): Observable<Product[]>{
+    getProductList(): Observable<Product[]>{ //returns an observable..Map the JSON data from SPring data Rest to product array
       return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
         map(response => response._embedded.products)
       );
@@ -21,7 +22,7 @@ export class ProductService {
   }
 
 
-interface GetResponse{
+interface GetResponse{ //unwraps the JSON from Srping Dat REST _embedded entry
   _embedded:{
     products:Product[];
   }
